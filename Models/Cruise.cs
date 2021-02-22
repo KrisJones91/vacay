c1f3fa7b65440433082324cdde0892657d2dc190
Kris Jones <kristop.jones@gmail.com>
2021-02-22 15:32:36 -0700

    inherit

--- Models/Cruise.cs
+++ Models/Cruise.cs
@@ -2,17 +2,17 @@ using System.ComponentModel.DataAnnotations;
 
 namespace vacay.Models
 {
-    public class Cruise
+    public class Cruise : Trip
     {
-        public int Id { get; set; }
+        // public int Id { get; set; }
+        // public string title { get; set; }
+        // public string description { get; set; }
         [Required]
-        public string title { get; set; }
-        public string description { get; set; }
         public string start { get; set; }
         public string end { get; set; }
 
         public int length { get; set; }
-        public float price { get; set; }
+        // public float price { get; set; }
 
     }
 }
--- Models/Rental.cs
+++ Models/Rental.cs
@@ -2,16 +2,16 @@ using System.ComponentModel.DataAnnotations;
 
 namespace vacay.Models
 {
-    public class Rental
+    public class Rental : Trip
     {
-        public int Id { get; set; }
+        // public int Id { get; set; }
         [Required]
         public string car { get; set; }
-        public string description { get; set; }
+        // public string description { get; set; }
         public int duration { get; set; }
 
         public float miles { get; set; }
 
-        public float price { get; set; }
+        // public float price { get; set; }
     }
 }
